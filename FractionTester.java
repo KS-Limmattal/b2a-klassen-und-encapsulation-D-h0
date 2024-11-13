5e6973a33d29b5fe2e2935fdf4f1323e1c15a58d
Daniel Holzner <daniel.c.holzner@gmail.com>
2024-11-13 08:06:30 +0100

    COMMIT_EDITMSG

--- FractionTester.java
+++ FractionTester.java
@@ -1,20 +1,24 @@
 public class FractionTester {
     public static void main(String[] args) {
-        Fraction f = new Fraction();
+        Fraction f = new Fraction(1, 2);
         // Der folgende Code funktioniert nur, wenn die Felder von Fraction nicht privat sind:
-        f.numerator = 12;
-        f.denominator = 0;
-        System.out.println(f.numerator + "/" + f.denominator);
+        // System.out.println(f.numerator + "/" + f.denominator);
         
         // toString():
+        System.out.println(f.toString());
 
         // Encapsulation, Getter und Setter:
+        f.setDenominator(3);
+        f.setNumerator(9);
 
         // Copy-Konstruktor und equals():
-        
+        System.out.println(f.equals(f));
+
         // Klassen- und Instanzenmethoden:
+        System.out.println(Fraction.numberOfFractions);
 
         // statische Variablen:
+        final double PI = 3.14159;
 
     }
 
